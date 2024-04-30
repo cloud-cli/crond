ef1756c47f639971620f4bf0b4f160d3724860c5
Darlan Alves <me@darlanalv.es>
2024-04-30 12:29:40 +0200

    fix: wrong cwd set to daemon process

--- daemon.mjs
+++ daemon.mjs
@@ -2,12 +2,13 @@
 
 import { spawn } from "child_process";
 import { openSync } from "fs";
-import { join } from "path";
+import { join, dirname } from "path";
 
 const logsFolder = process.env.CRON_LOGS_FOLDER || "/tmp/cronjobs";
 const stdout = openSync(join(logsFolder, "crond.log"), "a");
+const cwd = dirname(process.argv[1]);
 const child = spawn("node", ["index.mjs"], {
-  cwd: process.cwd(),
+  cwd,
   detached: true,
   stdio: ["ignore", stdout, stdout],
 });
